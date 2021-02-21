86b5cb549a8d65f56714772f75cd6ea7c1d099d5
marcelkaminski <marcel.kaminski.999@gmail.com>
2021-02-21 20:10:29 +0100

    Add Entry Page

--- Project 1/encyclopedia/views.py
+++ Project 1/encyclopedia/views.py
@@ -7,4 +7,8 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def getEntryPage(request, title):
+    return render(request, "encyclopedia/entryPage.html", {
+        "title": title,
+        "entry": util.get_entry(title)
+    })
