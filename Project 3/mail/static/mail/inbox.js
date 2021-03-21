14b407ee56d3375cd5bc4e3d82e1c8fe4a3577ed
marcelkaminski <marcel.kaminski.999@gmail.com>
2021-03-21 14:09:43 +0100

    Add Mailbox and View Mail

--- Project 3/mail/static/mail/inbox.js
+++ Project 3/mail/static/mail/inbox.js
@@ -35,17 +35,28 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
-      // Print emails
-      console.log(emails);
-
-      emails.forEach(mail => {
-          const element = document.createElement('div');
-          element.innerHTML = `${JSON.stringify(mail)}`;
-          //element.addEventListener('click', function() {
-          //    console.log('This element has been clicked!')
-          //});
-          document.querySelector('#emails-view').append(element);
+    emails.forEach(element => {
+      if (mailbox != "sent") {
+        sender_recipients = element.sender;
+      } else {
+        sender_recipients = element.recipients;
+      }
+      if (mailbox == "inbox") {
+        if (element.read) is_read = "read";
+        else is_read = "";
+      } else is_read = "";
+      var item = document.createElement("div");
+      item.className = `card   ${is_read} my-1 items`;
+      item.innerHTML = `<div class="card-body" id="item-${element.id}">
+          ${element.subject} | ${sender_recipients} | ${element.timestamp}
+          <br>
+          ${element.body.slice(0, 100)}
+          </div>`;
+      document.querySelector("#emails-view").appendChild(item);
+      item.addEventListener("click", () => {
+        show_mail(element.id, mailbox);
       });
+    });
   });
 }
 
@@ -66,4 +77,42 @@ function send_email(event) {
     }),
   })
   .then(load_mailbox("sent"));
+}
+
+function show_mail(id, mailbox) {
+  fetch(`/emails/${id}`)
+    .then((response) => response.json())
+    .then((email) => {
+      document.querySelector("#emails-view").innerHTML = "";
+      var item = document.createElement("div");
+      item.className = `card`;
+      item.innerHTML = `<div class="card-body" style="white-space: pre-wrap;">
+      Sender: ${email.sender}
+      Recipients: ${email.recipients}
+      Subject: ${email.subject}
+      Time: ${email.timestamp}
+      <br>${email.body}
+      </div>`;
+      document.querySelector("#emails-view").appendChild(item);
+      if (mailbox == "sent") return;
+      let archive = document.createElement("btn");
+      archive.className = `btn btn-outline-info my-2`;
+      archive.addEventListener("click", () => {
+        toggle_archive(id, email.archived);
+        if (archive.innerText == "Archive") archive.innerText = "Unarchive";
+        else archive.innerText = "Archive";
+      });
+      if (!email.archived) archive.textContent = "Archive";
+      else archive.textContent = "Unarchive";
+      document.querySelector("#emails-view").appendChild(archive);
+
+      let reply = document.createElement("btn");
+      reply.className = `btn btn-outline-success m-2`;
+      reply.textContent = "Reply";
+      reply.addEventListener("click", () => {
+        reply_mail(email.sender, email.subject, email.body, email.timestamp);
+      });
+      document.querySelector("#emails-view").appendChild(reply);
+      make_read(id);
+    });
 }
